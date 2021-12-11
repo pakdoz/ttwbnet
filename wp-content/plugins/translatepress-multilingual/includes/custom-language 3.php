644657bea8f926c0b701e493cac4eb6a3c4caed0
pakdoz <ferdian.alfianto@icloud.com>
2021-12-11 10:14:49 +0700

    csv to html

---
+++ wp-content/plugins/translatepress-multilingual/includes/custom-language 3.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_image_size() in /www/wwwroot/tataweb.net/wp-content/plugins/translatepress-multilingual/includes/custom-language.php:2
+Stack trace:
+#0 {main}
+  thrown in <b>/www/wwwroot/tataweb.net/wp-content/plugins/translatepress-multilingual/includes/custom-language.php</b> on line <b>2</b><br>
---
+++ wp-content/plugins/translatepress-multilingual/includes/google-translate/functions 3.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_filter() in /www/wwwroot/tataweb.net/wp-content/plugins/translatepress-multilingual/includes/google-translate/functions.php:3
+Stack trace:
+#0 {main}
+  thrown in <b>/www/wwwroot/tataweb.net/wp-content/plugins/translatepress-multilingual/includes/google-translate/functions.php</b> on line <b>3</b><br>
